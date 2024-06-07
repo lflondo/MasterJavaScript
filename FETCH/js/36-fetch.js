e2c6c0b40212e89d192b69f19d46c10a827bb083
NOLFLONDONO <lflondono@novaventa.com>
2024-06-07 17:45:53 -0500

    commiting 20240607

--- FETCH/js/36-fetch.js
+++ FETCH/js/36-fetch.js
@@ -5,23 +5,60 @@
 
 
 var div_usuarios = document.querySelector("#usuarios");
+var div_janet = document.querySelector("#janet");
 
-var usuarios = [];
+    getUsuarios()
+        .then(data => data.json())
+        .then(users=> {
+            listadoUsuarios(users.data);
 
-fetch('https://reqres.in/api/users')
-    .then(data => data.json())
-    .then(users=> {
-        usuarios = users.data;
-        console.log(usuarios);
+            return getJanet();
+            
+        })
+        .then (data => data.json())
+        .then (user2 =>{
+            mostrarJanet(user2.data);
 
-        usuarios.map((user, i) => {
-            let nombre = document.createElement('h3');
-    
-            nombre.innerHTML = i +". "+ user.first_name + " " + user.last_name;
+        })
 
-            div_usuarios.appendChild(nombre);
 
-            document.querySelector(".loading").style.display = 'none';
-        });
+function getUsuarios(){
+    return fetch('https://reqres.in/api/users');
+
+}
+
+function  getJanet(){
+
+    return fetch('https://reqres.in/api/users/2');
+}
+
+function listadoUsuarios(usuarios){
+    usuarios.map((user, i) => {
+        let nombre = document.createElement('h3');
+
+        nombre.innerHTML = i +". "+ user.first_name + " " + user.last_name;
+
+        div_usuarios.appendChild(nombre);
+
+        document.querySelector(".loading").style.display = 'none';
+    });
+}
+
+
+function mostrarJanet(user){ 
+   
+        let nombre = document.createElement('h4');
+
+        let avatar = document.createElement('img');
+
+        nombre.innerHTML = user.first_name + " " + user.last_name;
+
+        avatar.src = user.avatar;
+        avatar.width = '100';
+
+        div_janet.appendChild(avatar);
+        div_janet.appendChild(nombre);
         
-    })
+
+        document.querySelector("#janet .loading").style.display = 'none';
+}
