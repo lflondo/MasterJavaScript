c021e5d21f5cb0502239a47b04ee4a81010116da
llondo <luis.londob81@gmail.com>
2024-06-12 19:36:16 -0500

    actializacion 20240612

---
+++ FETCH/js/37-try-catch.js
@@ -0,0 +1,18 @@
+'use strict';
+
+try{
+    var year = 2019;
+    alert(year);
+
+    var vector = new Array(9999999999999999)
+
+    console.log(vector)
+
+    //console.log(decodeURIComponent("https://github.com/dashboard"));
+
+}catch(error){
+    console.log(error)
+    alert("A ocurrido un error en el código")
+}
+
+
