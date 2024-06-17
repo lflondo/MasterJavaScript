86c35d45ea33a985dfa98b78c44d088894b12d59
NOLFLONDONO <lflondono@novaventa.com>
2024-06-17 08:51:06 -0500

    actualizacion 20240617

---
+++ JQuery/jsmin/01-selectores.js
@@ -0,0 +1,9 @@
+'use strict'
+
+$(document).ready(function(){
+    //Selector de ID
+   var rojo = $("#rojo").css("background","red")
+                        .css("color","white");
+   
+   console.log(rojo);
+})
