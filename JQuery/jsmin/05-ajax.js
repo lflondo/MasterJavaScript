83d269732b8c7ba0b10fcfd2fe415cae3b213764
llondo <luis.londob81@gmail.com>
2024-07-08 17:09:37 -0500

    actualizacion 20240708

--- JQuery/jsmin/05-ajax.js
+++ JQuery/jsmin/05-ajax.js
@@ -2,5 +2,14 @@
 
 $(document).ready(function(){
     //Load
-    $('#datosajax').load("https://reqres.in/");
+    // $('#datosajax').load("https://reqres.in/");
+
+    // metodo Get y Post
+
+    $.get("https://reqres.in/api/users", {page: 2}, function(response){
+        response.data.forEach((element, index) => {
+            $('#datosajax').append("<p>"+element.first_name +" "+ element.last_name+"</p>")
+        });
+    })
+
 });
