bda295414a9482236c146555e4ab2a96f573bd7f
llondo <luis.londob81@gmail.com>
2024-07-28 17:52:11 -0500

    actualizacion 20240728

--- JQuery/jsmin/06-interface.js
+++ JQuery/jsmin/06-interface.js
@@ -20,4 +20,27 @@ $(document).ready(function(){
         }
     });
 
+    //Drop
+    $("#movido").draggable();
+    $("#area").droppable({
+        drop: function(){
+            console.log("has soltado algo dentro del area")
+        }
+    });
+
+    //Efectos
+    $("#mostrar").click(function(){
+    //$(".caja-efectos").fadeToggle()
+     //$(".caja-efectos").toggle("fade")    
+    //$(".caja-efectos").effect("explode")
+    //$(".caja-efectos").toggle("explode") 
+    //$(".caja-efectos").toggle("blink")
+    //$(".caja-efectos").toggle("blind")
+    //$(".caja-efectos").toggle("slide")
+    //$(".caja-efectos").toggle("drop") 
+    //$(".caja-efectos").toggle("fold")
+    //$(".caja-efectos").toggle("puff")
+    //$(".caja-efectos").toggle("scale")
+    $(".caja-efectos").toggle("shake", 4000)   
+    });
 });
