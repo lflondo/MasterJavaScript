9c70f2f5f5bfa391c0a3cef7fe2ceddc18940a06
llondo <luis.londob81@gmail.com>
2025-02-04 22:04:53 -0500

    acualizacion 20250204_2204

--- proyecto-js/js/main.js
+++ proyecto-js/js/main.js
@@ -78,5 +78,43 @@ $(document).ready(function(){
         theme.attr("href", "css/blue.css");
         localStorage.setItem('selectedTheme', 'blue');
     });
+
+    //Scroll arriba de la web
+
+
+    $('.subir').click(function(e){
+        e.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+
+    //login falso
+
+    $('#login').submit(function(){
+        
+        var form_name = $("#form_name").val();
+
+        localStorage.setItem("form_name", form_name);
+    });
+
+    var form_name = localStorage.getItem("form_name");
+
+    if(form_name != null && form_name != "undefined"){
+        var about_parrafo = $("#about p")
+
+        about_parrafo.html("</br><strong>Bienvenido, "+form_name+" </strong>");
+        about_parrafo.append("<a href='#' id='logout'>Cerrar sesión</a>")
+
+        $("#login").hide();
+
+        $("#logout").click(function(){
+            localStorage.clear();
+            location.reload();
+        });
+    };
+
     
 });
