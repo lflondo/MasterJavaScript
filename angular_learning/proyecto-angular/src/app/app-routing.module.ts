7f0ca71d1f1bab80873e288fcdaae448c8ea0e99
llondo <luis.londob81@gmail.com>
2025-03-22 22:01:59 -0500

    actualizacion 20250322_22:01

--- angular_learning/proyecto-angular/src/app/app-routing.module.ts
+++ angular_learning/proyecto-angular/src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ModuleWithProviders } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+// Importar compnentes
+
+import { HomeComponent } from './home/home.component';
+import { ZapatillasComponent } from './zapatillas/zapatillas.component';
+import { VideojuegoComponent } from './videojuego/videojuego.component';
+import { CursosComponent } from './cursos/cursos.component';
+
+// Array de rutas
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'zapatillas', component: ZapatillasComponent},
+  {path: 'videojuego', component: VideojuegoComponent},
+  {path: 'cursos', component: CursosComponent},
+  {path: '**', component: HomeComponent}
+];
+
+//Exportar el modulo de rutas
+
+export const AppRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders<AppRoutingModule> = RouterModule.forRoot(appRoutes);
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- angular_learning/proyecto-angular/src/app/app.module.ts
+++ angular_learning/proyecto-angular/src/app/app.module.ts
@@ -2,25 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
+import {routing, AppRoutingProviders} from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { VideojuegoComponent } from './videojuego/videojuego.component';
 import { ZapatillasComponent } from './zapatillas/zapatillas.component';
 import { CursosComponent } from './cursos/cursos.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     VideojuegoComponent,
     ZapatillasComponent,
-    CursosComponent
+    CursosComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    routing
   ],
-  providers: [],
+  providers: [AppRoutingProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
