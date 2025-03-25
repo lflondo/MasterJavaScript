1f1414f4a0040691278d402e621d9ca2a1f5c48c
llondo <luis.londob81@gmail.com>
2025-03-24 22:10:07 -0500

    actualizacion 20250324_22:09

--- angular_learning/proyecto-angular/src/app/app-routing.module.ts
+++ angular_learning/proyecto-angular/src/app/app-routing.module.ts
@@ -12,9 +12,11 @@ import { CursosComponent } from './cursos/cursos.component';
 // Array de rutas
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
   {path: 'zapatillas', component: ZapatillasComponent},
   {path: 'videojuego', component: VideojuegoComponent},
   {path: 'cursos', component: CursosComponent},
+  {path: 'cursos/:nombre/:followers', component: CursosComponent},
   {path: '**', component: HomeComponent}
 ];
 
--- angular_learning/proyecto-angular/src/app/cursos/cursos.component.ts
+++ angular_learning/proyecto-angular/src/app/cursos/cursos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'cursos',
@@ -6,11 +7,24 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './cursos.component.html',
   styleUrl: './cursos.component.css'
 })
-export class CursosComponent {
-  constructor(){
+export class CursosComponent implements OnInit {
+  public nombre!: string;
+  public followers: number;
+
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router,
+  ){
+    this.followers=0;
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this._route.params.subscribe((params: Params) => {
+      this.nombre = params['nombre'];
+      this.followers = +params['followers'];
+      // console.log(this.nombre);
+      // console.log(typeof +params['followers']);
+    });
   }
 
 }
